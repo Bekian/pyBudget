cf48bc008abb2453b1fa8eff582d93e4f27fc21e
Bekian <Bekian.Cat@gmail.com>
2025-03-15 22:29:03 -0500

    feat(app): added description and simple help cmd
    added description that i already created on the github, added a data folder that i'll use for my own local data (idrk how i could integrate this for the users, maybe some config import tools/help?), added a lil help command with some inspect/reflection features to print docs (ill use this later to help me load cmd docs for the help function)

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+import sys
+import inspect
+
+
+def help(cmd: str = ""):
+    """
+    This help function gives instruction on how to use the application.
+
+    Usage:
+        pyBudget [cmd] [options]
+
+    Commands:
+        help [cmd] [option]
+
+    Args:
+        cmd: optionally display specific command usage.
+        If no cmd argument is provided, this message is displayed.
+    """
+    if cmd:
+        match cmd:
+            case "repl" | "r":
+                print('repl sample help output')
+            case _:
+                print("Command option not found, try again.")
+    else:
+        print(inspect.getdoc(help))
+
+
 def main():
-    print("Hello from pybudget!")
+    args = sys.argv
+    # debug
+    # TODO: MAKE THIS A RUN OPTION
+    print("\n~~~  DEBUG PRINT START ~~~")
+    print('args: ', args)
+    print('args len: ', args.__len__())
+    print("~~~  DEBUG PRINT END ~~~\n")
+    # check which args option was used
+    if args.__len__() == 1:
+        help()
+    elif args.__len__() > 1:
+        match args[1]:
+            case "h" | "help":
+                help()
 
 
 if __name__ == "__main__":
